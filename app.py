c81fe29b3ddd3916fb70f05a87665e300e1e25dc
AmbNightmare <alan.monroy@ucsp.edu.pe>
2021-10-19 19:43:21 -0500

    SQL Test

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from datetime import datetime
+from markupsafe import escape
+
 from flask import Flask
 from flask.templating import render_template
+from flask_sqlalchemy import SQLAlchemy, SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/sge.db'
+
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
 
 @app.route("/")
 def index():
