ad6ccbf7dae3b08b64c6dd7476d3018ff77fb5dd
AmbNightmare <alan.monroy@ucsp.edu.pe>
2021-10-20 00:26:58 -0500

    Registro guarda en BD

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from markupsafe import escape
 
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy, SQLAlchemy
 
@@ -15,22 +15,44 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
+    nombre = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     tipodoc = db.Column(db.String(10), nullable=False)
     doc = db.Column(db.String(10), nullable=False)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html", texto="las plantillas")
+    return render_template('index.html', texto='las plantillas')
 
-@app.route("/login")
+@app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
 
-@app.route("/signup")
+@app.route('/signup')
 def register():
-    return render_template("signup.html")
+    return render_template('signup.html')
+
+@app.route('/create-user', methods=['POST'])
+def create_user():
+    user = Usuario.query.filter_by(username = request.form.get('usuario')).first()
+    mail = Usuario.query.filter_by(email = request.form.get('correo')).first()
+    if user:
+        return 'Usuario ya existente'
+    if mail:
+        return 'El correo ingresado ya tiene una cuenta asociada'
+    nuevo_usuario = Usuario(
+        username = request.form.get('usuario'),
+        password = request.form.get('contra'),
+        nombre = request.form.get('nombre'),
+        email = request.form.get('correo'),
+        tipodoc = request.form.get('tipo_doc'),
+        doc = request.form.get('doc')
+    )
+    db.session.add(nuevo_usuario)
+    db.session.commit()
+    print(Usuario.query.all())
+    return redirect(url_for('login'))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
